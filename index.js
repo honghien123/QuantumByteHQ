9e4bce84fbe0ab03107774305f343dec12916269
honghien123 <cameron.rnpun0suy@hotmail.com>
2024-05-12 05:18:37 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function searchInsert(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[mid] < target) left = mid + 1;
-    else right = mid - 1;
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
   }
-  return left;
-}
+  return arr;
+};
