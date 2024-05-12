089e0c5f491840429efc0e1087cc6a421be6866a
honghien123 <cameron.rnpun0suy@hotmail.com>
2024-05-12 22:02:09 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const insertionSort = (arr) => {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
     }
-    arr[j + 1] = key;
+    prefix = prefix.substring(0, j);
   }
-  return arr;
-};
+  return prefix;
+}
