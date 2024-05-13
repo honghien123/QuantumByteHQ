464da5a8d9d5d2690dfae5633f981a0c6e337e82
honghien123 <cameron.rnpun0suy@hotmail.com>
2024-05-14 02:19:12 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-function longestCommonPrefix(strs) {
-  if (strs.length === 0) return "";
-  let prefix = strs[0];
-  for (let i = 1; i < strs.length; i++) {
-    let j = 0;
-    while (
-      j < prefix.length &&
-      j < strs[i].length &&
-      prefix.charAt(j) === strs[i].charAt(j)
-    ) {
-      j++;
-    }
-    prefix = prefix.substring(0, j);
+function mergeKLists(lists) {
+  if (lists.length === 0) return null;
+  while (lists.length > 1) {
+    const first = lists.shift();
+    const second = lists.shift();
+    const merged = mergeTwoLists(first, second);
+    lists.push(merged);
   }
-  return prefix;
+  return lists[0];
 }
